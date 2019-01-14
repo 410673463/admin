679016b20778da7712f91e9900a125964200640b
410673463 <410673463@163.com>
2019-01-14 17:02:29 +0800

    cloud加依赖

--- admin-spring-cloud-server/src/main/java/com/admin/springcloud/AdminSpringCloudApplication.java
+++ admin-spring-cloud-server/src/main/java/com/admin/springcloud/AdminSpringCloudApplication.java
@@ -2,8 +2,10 @@ package com.admin.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class AdminSpringCloudApplication {
 
     public static void main(String[] args) {
