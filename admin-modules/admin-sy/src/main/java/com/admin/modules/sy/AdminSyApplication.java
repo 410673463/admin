5489e929f5d6a8abf5a99a6d5fe9acddd3713ea3
410673463 <410673463@163.com>
2019-01-14 16:29:34 +0800

    添加子项目2

---
+++ admin-modules/admin-sy/src/main/java/com/admin/modules/sy/AdminSyApplication.java
@@ -0,0 +1,14 @@
+package com.admin.modules.sy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminSyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminSyApplication.class, args);
+    }
+
+}
+
