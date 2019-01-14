7e1b0e3cecd73e0952dc42a2d7c741aed5e40952
410673463 <410673463@163.com>
2019-01-14 15:38:46 +0800

    目录结构首次导入

---
+++ admin-spring-cloud-server/src/main/java/com/admin/springcloud/AdminSpringCloudApplication.java
@@ -0,0 +1,14 @@
+package com.admin.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminSpringCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminSpringCloudApplication.class, args);
+    }
+
+}
+
