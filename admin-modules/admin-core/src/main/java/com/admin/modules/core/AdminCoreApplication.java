8f206d1f3a64facac65ab6b457bd7d2a4d8b0bfe
410673463 <410673463@163.com>
2019-01-15 14:42:31 +0800

    删除springcloudconfig项目，庆祝core项目启动成功，付俊男是笨蛋

--- admin-modules/admin-core/src/main/java/com/admin/modules/core/AdminCoreApplication.java
+++ admin-modules/admin-core/src/main/java/com/admin/modules/core/AdminCoreApplication.java
@@ -2,8 +2,11 @@ package com.admin.modules.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AdminCoreApplication {
 
     public static void main(String[] args) {
