2be5daed91e78e1ebccd659d30678ad6d35f83df
410673463 <410673463@163.com>
2019-01-17 15:11:12 +0800

    gateway模块

---
+++ admin-gateway/src/main/java/com/admin/gateway/FallbackController.java
@@ -0,0 +1,16 @@
+package com.admin.gateway;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/fallback")
+public class FallbackController {
+
+
+    @RequestMapping("")
+    public String fallback(){
+        return "error";
+    }
+}
