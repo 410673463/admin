424011b725587adbec27025f0ca10b3471e4c364
410673463 <410673463@163.com>
2019-01-17 16:10:45 +0800

    gateway模块增加断路器功能

--- admin-gateway/src/main/java/com/admin/gateway/AdminGatewayApplication.java
+++ admin-gateway/src/main/java/com/admin/gateway/AdminGatewayApplication.java
@@ -11,6 +11,20 @@ public class AdminGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminGatewayApplication.class, args);
     }
+  /*  @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        StripPrefixGatewayFilterFactory.Config config = new StripPrefixGatewayFilterFactory.Config();
+        config.setParts(1);
+        return builder.routes()
+                .route(
+                        r -> r.path("/admin-core/**")//路由条件
+                        .filters(f -> f.stripPrefix(1).filter(new Hystrix()))//过滤器
+                        .uri("lb://admin-core")//目标服务地址
+                        .order(0)//排序
+                        .id("admin-core")
+                )
+                .build();
+    }*/
 
 }
 
